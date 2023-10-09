832bdadf84c3f0720f5b2c205838d1300eb4e173
Ola Folarin <olafolarin@Olas-MBP.attlocal.net>
2023-10-08 20:05:23 -0500

    removed exposed credntials n specify subnet n vpc in module

--- myresource.tf
+++ myresource.tf
@@ -25,7 +25,9 @@ variable "AWS_SECRET_ACCESS_KEY" {}
 resource "aws_instance" "web1" {
     ami = "ami-067d1e60475437da2"
     instance_type = "t2.micro"
+   vpc_id     = aws_vpc.main.id
    subnet_id = aws_subnet.publ.id
+   cidr_block = "10.1.0.0/24"
 
     tags = {
       Name = "myinstance1"
