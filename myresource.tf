814b8a2846767bd41375f39f83552cabe5e5d94e
Ola Folarin <olafolarin@Olas-MBP.attlocal.net>
2023-10-08 20:27:47 -0500

    removed exposed credntials n removed specify custom subnet arn

--- myresource.tf
+++ myresource.tf
@@ -25,7 +25,7 @@ variable "AWS_SECRET_ACCESS_KEY" {}
 resource "aws_instance" "web1" {
     ami = "ami-067d1e60475437da2"
     instance_type = "t2.micro"
-    subnet_id = "arn:aws:ec2:us-east-1:630267599246:subnet/subnet-0434b26a52489c0c1"
+    subnet_id = "arn:aws:ec2:us-east-1:630267599246:subnet/subnet-0b620fa451b0e730f"
 
     tags = {
       Name = "myinstance1"
