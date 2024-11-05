9dbba6662f1a46d50edd91e9310657cc5dad06e4
Ola Folarin <olafolarin@Olas-MBP.lan>
2024-11-04 21:11:52 -0600

    deleted new.var2.tf and modified var2.tf and myresource.tf to pass in values for variable defined for AMI and Instance Type

--- var2.tf
+++ var2.tf
@@ -3,3 +3,9 @@ variable "AMI" {
     description = "The ID of the Amazon Machine Image (AMI) to use for the instance."
     default = "ami-067d1e60475437da2"
 }
+
+variable "instance_type" {
+    type        = string
+    description = "The  type of instance to start."
+    default = "t2.micro"
+}
